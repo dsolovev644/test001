ee001e80936e6966e988fd7e6b1a858e0ff89eba
Denis Solovev <dsolovev829@gmail.com>
2025-04-27 09:25:29 -0400

    added 2

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,14 @@
 #include <iostream>
 #include "test001/example.h"
 
+#include <boost/filesystem.hpp>
+#include <spdlog/spdlog.h>
+
 int main() {
     std::cout << "Hello, Modern C++!\n";
     example();
+
+    boost::filesystem::path p{"."};
+    spdlog::info("Current directory is {}", p.string());
     return 0;
 }
